c3ab8a2c1aafe6277b5b79d6828d89754939c13f
daferrom <daferrom@gmail.com>
2021-09-29 10:28:38 -0500

    server config keys and request in process

--- server/src/database.ts
+++ server/src/database.ts
@@ -5,3 +5,10 @@ import keys from './keys'
 
 const pool = mysql.createPool(keys.database);
 
+pool.getConnection()
+    .then(connection => {
+        pool.releaseConection(connection);
+        console.log('DB is connected')
+    });
+
+export default pool;

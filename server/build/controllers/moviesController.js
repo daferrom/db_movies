81536269f7f296a5822b70f3b1d423b09a025152
daferrom <daferrom@gmail.com>
2021-09-30 00:24:23 -0500

    checkpoint midnight 30 sep

--- server/build/controllers/moviesController.js
+++ server/build/controllers/moviesController.js
@@ -1,9 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.moviesController = void 0;
+const database_1 = __importDefault(require("../database"));
 class MoviesController {
     index(req, res) {
-        /*pool.query('DESCRIBE movies');*/
+        database_1.default.query('DESCRIBE movies');
         res.json('movies');
     }
 }

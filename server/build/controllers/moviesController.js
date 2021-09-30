755aebf0bc713e635db52b235da01404475371c2
Laura bogota <lauralejab96@gmail.com>
2021-09-30 15:39:37 -0500

    actualizacionramaprincipal

--- server/build/controllers/moviesController.js
+++ server/build/controllers/moviesController.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.moviesController = void 0;
 class MoviesController {
     index(req, res) {
-        /*pool.query('DESCRIBE movies');*/
+        pool.query('DESCRIBE movies');
         res.json('movies');
     }
 }

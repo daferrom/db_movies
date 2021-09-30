e63ca9cb6903952ab01f1db77d326e89bfd46771
daferrom <daferrom@gmail.com>
2021-09-29 20:16:42 -0500

    data of database config updated

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -2,19 +2,11 @@ export default {
 
     database:{
         // it specifies where it is the data base (an IP adress)//
-<<<<<<< HEAD
-        host:'localhost',
-        user: 'root',
-        password: 'Simonides1927+',
-        database: 'video_club_db',
-        //port: '3000'
-=======
         host:'bfin8x7v3anck6tfjuip-mysql.services.clever-cloud.com',
         user: 'udvvgpee1t4iulpf',
         password: 'RNX0xh0nY79v0GuM0EF2',
         database: 'bfin8x7v3anck6tfjuip',
         //port: '3306'
->>>>>>> 27057caa12b6a6a5e7f5d3b5276fb0c7e0cad8a3
     }
 
 }

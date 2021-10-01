f63a2ea5f1c3afdbd62beaf27e496c6b88154351
daferrom <daferrom@gmail.com>
2021-10-01 01:02:26 -0500

    some files corrected

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,11 +6,7 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { MovieFormComponent } from './components/movie-form/movie-form.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
-<<<<<<< HEAD
 import { MoviesService } from './services/movies.service'
-=======
-import { MoviesService} from './services/movies.service'
->>>>>>> 3423a8d10a7e972456111b7f7f9c0230921cbc2d
 
 @NgModule({
   declarations: [
